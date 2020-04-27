7f971ec428e0a8489f915f148a89a02b966be946
cloud7sbs <cloud7bene@naver.com>
2020-04-27 14:09:38 +0900

    Map Terrain 수정, Enemy 추가
    Map Terrain 수정 , 효과 재적용, Enemy 추가 이동 공격, 타이틀씬, Start씬 수정중

---
+++ NightFury/Assets/SBS/Scripts/SBS_D_Enemy.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; //NavMeshAgent 사용
+public class SBS_D_Enemy : MonoBehaviour
+{
+    //목표 : Enemy Dragon 만들고싶다
+    //필요속성 : 대기시간,현재시간,스피드,에니메이터,공격범위,NavMesh에이전트,캐릭터컨트롤,현재hp,사운드
+
+    public List<Transform> enemy_WayPoints;
+    public float waitTime = 1.7f;
+    float currentTime = 0;
+    float rotSpeed = 4;
+    public enum eState
+    {
+        PATROL,
+        WAIT,
+        MOVE,
+        ATTACK,
+        DAMAGE,
+        DIE
+    }
+    public eState state;
+    public int e_Hp = 4;
+    public float dmgDelayTime = 1.3f;
+    public float moveSpeed = 25.0f;
+    public float atkRange = 1000.0f;
+    public float atkDelayTime = 1.3f;
+    float atkClipLen = 0.0f;
+    public Transform target;
+    public GameObject enemyBreath;
+    public Transform breathTr;
+    CharacterController charCon;
+    Animator animator;
+    NavMeshAgent agent;
+    AudioSource soundP;
+    public AudioClip die_S;
+    //public AudioClip atk_S;
+    void Start()
+    {
+        target = GameObject.Find("NightFury").transform;
+        state = eState.WAIT;
+        charCon = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        soundP = GetComponent<AudioSource>();
+        agent.enabled = false;
+    }
+
+    void Update()
+    {
+        switch (state)
+        {
+            case eState.WAIT:
+                Wait();
+                break;
+            case eState.MOVE:
+                Move();
+                break;
+            case eState.ATTACK:
+                Attack();
+                break;
+            case eState.DAMAGE:
+                break;
+            case eState.DIE:
+                break;
+        }
+    }
+
+    //대기
+    void Wait()
+    {
+        currentTime += Time.deltaTime;
+        if (currentTime > waitTime)
+        {
+            state = eState.MOVE;
+            animator.SetTrigger("Move");
+            currentTime = 0;
+        }
+
+    }
+
+    //이동 처리
+    void Move()
+    {
+        // charCon.SimpleMove(dir.normalized * moveSpeed);
+        Vector3 dir = target.transform.position - transform.position;
+
+        if (agent.enabled == false) agent.enabled = true;
+
+        agent.destination = target.transform.position;
+        dir.y = 0;
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir.normalized), rotSpeed * Time.deltaTime);
+
+        if (dir.sqrMagnitude < atkRange)
+        {
+            agent.enabled = false;
+            state = eState.ATTACK;
+            currentTime = atkDelayTime + atkClipLen;
+        }
+    }
+
+    // 공격상태
+    void Attack()
+    {
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        int attackState = Animator.StringToHash("Base Layer.Attack.Attack");
+
+        if (stateInfo.fullPathHash == attackState) atkClipLen = stateInfo.length;
+
+        currentTime += Time.deltaTime;
+
+        if (currentTime > atkDelayTime + atkClipLen)
+        {
+            GameObject magic = Instantiate(enemyBreath);
+            magic.transform.position = breathTr.position;
+           // magic.transform.LookAt(target.transform.position);
+
+            animator.SetTrigger("Attack");
+            currentTime = 0.0f;
+            // SBS_PlayerHealth.instance.currHp--;
+            // SBS_PlayerHealth.instance.DisplayHpbar();
+
+
+        }
+
+        Vector3 dir = target.transform.position - transform.position;
+
+        if (dir.sqrMagnitude > atkRange)
+        {
+            animator.SetTrigger("Move");
+            state = eState.MOVE;
+        }
+    }
+    //공격범위Show
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, atkRange);
+        Gizmos.color = new Color(1.1f, 0.1f, 1.1f, 0.4f);
+    }
+
+    // Dmg,Die 코루틴
+    public IEnumerator DamageProcess()
+    {
+        e_Hp--;
+
+        if (e_Hp > 0)
+        {
+            //Damage
+            state = eState.DAMAGE;
+            animator.SetTrigger("Damage");
+            yield return new WaitForSeconds(dmgDelayTime);
+            state = eState.WAIT;
+        }
+        else
+        {
+            //Die
+            Die();
+        }
+        yield return null;
+    }
+    //죽음처리
+    private void Die()
+    {
+        soundP.PlayOneShot(die_S); //죽음 사운드
+        state = eState.DIE;
+        animator.SetTrigger("Die");
+        Destroy(gameObject, 1.5f);
+    }
+}
---
+++ NightFury/Assets/SBS/Scripts/SBS_GameMgr.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SBS_GameMgr : MonoBehaviour
+{
+
+    //목표: 매니져를 생성하고 접근하여 데이터를 쉽게 쓰고싶다
+
+    //필요속성 : 인스턴스, 적생성위치, 적생성시간,적생성수,스코어,게임종료
+
+    private static SBS_GameMgr instance;
+    public static SBS_GameMgr Instance
+    {
+        get
+        {
+            if (instance == null) instance = FindObjectOfType<SBS_GameMgr>();
+            
+            return instance;
+        }
+    }
+
+    private int score;
+
+    public float createTime = 7.0f;
+
+    public int maxEnemy = 7;
+
+    public bool _isGameBossDie = false;
+
+    public bool _isEnemyHqDestroy = false;
+    public bool _isGameover { get; private set; }
+
+    public Transform[] e_PointsTr;
+
+    public Transform[] e_PointsTr2;
+
+    public GameObject enemys;
+    private void Awake()
+    {
+        if (Instance != this) Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        e_PointsTr = GameObject.Find("E_SpawnPoints").GetComponentsInChildren<Transform>();
+      
+        if (e_PointsTr.Length > 0) StartCoroutine(this.CreateEnemy());
+
+        e_PointsTr2 = GameObject.Find("E_SpawnPoints2").GetComponentsInChildren<Transform>();
+
+        if (e_PointsTr2.Length > 0) StartCoroutine(this.CreateEnemy());
+    }
+
+    // Enemy 생성
+    IEnumerator CreateEnemy()
+    {
+        while (!_isGameover)
+        {
+            int enemyCount = (int)GameObject.FindGameObjectsWithTag("ENEMY").Length;
+
+            if (enemyCount < maxEnemy)
+            {
+ 
+                yield return new WaitForSeconds(createTime);
+
+                int idx = Random.Range(1, e_PointsTr.Length);
+
+                Instantiate(enemys, e_PointsTr[idx].position, e_PointsTr[idx].rotation);
+
+                int idx2 = Random.Range(1, e_PointsTr2.Length);
+
+                Instantiate(enemys, e_PointsTr2[idx].position, e_PointsTr2[idx].rotation);
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+    }
+
+    public void AddScore(int newScore)
+    {
+        if (!_isGameover)
+        {
+            score += newScore;
+          //  SBS_UIManager.Instance.UpdateScoreText(score);
+        }
+    }
+    
+    public void EndGame()
+    {
+        _isGameover = true;
+       // SBS_UIManager.Instance.SetActiveGameoverUI(true);
+    }
+}
