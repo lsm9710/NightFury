9eb0c2d0439db16d1193d5eeeafaea35fc9f2d2b
lsm9710 <54009854+lsm9710@users.noreply.github.com>
2020-05-08 17:48:57 +0900

    알고리즘맵 제작중

--- NightFury/Assets/SMFolder/Scripts/Algorithm Map/RandomScale.cs
+++ NightFury/Assets/SMFolder/Scripts/Algorithm Map/RandomScale.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -23,26 +24,70 @@ public class RandomScale : MonoBehaviour
     //포지션을 붙이면서 리스트에 추가
     //위아래 따로 리스트관리
     public List<GameObject> blockPool = new List<GameObject>();
+    public List<GameObject> topBlockPool = new List<GameObject>();
+    public List<GameObject> bottomBlockPool = new List<GameObject>();
 
     //플레이어의 높이와 부모 오브젝트의 높이차 비교
     // Start is called before the first frame update
     void Start()
     {
         blockFactory = Resources.Load<GameObject>("Algorithm Map/TestCube");
-        float a = Random.Range(min, max);
-        //오브젝트 풀로 생성하자
-        for (int i = 0; i < amount/2; i++)
+        
+        RandomScaleInstant();
+        SetPositionInPillar();
+    }
+
+    //위로 쌓아올릴 큐브들을 집어넣을 배열
+    //GameObject[] StackingUp;
+    private void SetPositionInPillar()
+    {
+        //위로 쌓을녀석들을 찾아오는 문장
+        //StackingUp = GameObject.FindGameObjectsWithTag("Cube");
+        for (int i = 0; i < blockPool.Count / 2 - 1; i++)
         {
-            //뭘 생성할건데? => 블록 => 블록은 뭔데?
-            block = Instantiate(blockFactory);
-            blockPool.Add(block);
-            block.transform.SetParent(pillar);
+            //인덱스 2번의 로컬스케일 Y를 둘로 나눈값을 저장한다
+            float a = blockPool[i].transform.localScale.y / 2;
+            //인덱스 3번의 로컬스케일 Y를 둘로 나눈값을 저장한다
+            float b = blockPool[i + 1].transform.localScale.y / 2;
+            //새로 지정해줄 y포지션값을 저장할 변수
+            Vector3 topPosition = Vector3.up * (a + b);
+
+            topBlockPool.Add(blockPool[i]);
+
+            blockPool[0].transform.position = blockPool[0].transform.parent.position;
+            //3번의 포지션을 2번과 똑같게 하고
+            blockPool[i + 1].transform.position = blockPool[i].transform.position;
+            //3번의 Y만 2개의 값을 둘로 나눠 합친값 만큼 더해준다
+            blockPool[i + 1].transform.position += topPosition;
+            blockPool[i].transform.name = i.ToString();
+            //만약에 Length보다 커졌으면 그만
+            if (blockPool.Count < i + 1) break;
+        }
+        for (int u = 0; u < blockPool.Count; u++)
+        {
+
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    void RandomScaleInstant()
     {
+        //오브젝트 풀로 생성하자
+        for (int q = 0; q < amount; q++)
+        {
+            //랜덤한 가로,세로,높이의 크기를 구하고
+            float x = UnityEngine.Random.Range(min, max);
+            float y = UnityEngine.Random.Range(min, max);
+            float z = UnityEngine.Random.Range(min, max);
+            //뭘 생성할건데? => 블록 => 블록은 뭔데?
+            //블록공장에서 생산
+            block = Instantiate(blockFactory);
+            //생산한녀석의 크기를 랜덤하게 지정해주고
+            block.transform.localScale = new Vector3(x, y, z);
+            //리스트에 집어넣고
+            blockPool.Add(block);
+            //부모를 기둥으로 지정
+            block.transform.SetParent(pillar);
+        }
         
     }
 }
--- NightFury/Assets/SMFolder/Scripts/Algorithm Map/gustnWKd123.cs
+++ NightFury/Assets/SMFolder/Scripts/Algorithm Map/gustnWKd123.cs
@@ -4,63 +4,29 @@ using UnityEngine;
 
 
 
-/// 생성 할 부\ㅡㄹ록 갯수 정해 == 작수여야해
-/// 블록 가로세로 리미트 정하고 그걸로 랜덤 때릴거임
-/// 기둥쓰 한개당 몇개 박을거냐?!
-/// 뭐부터 해볼까
-/// 랜덤으로 키워서 그거 위에 붙이는거부터 하나 해보께오
 
-
-
-
-
-//그냥 하이어라키에 있는애들 이름이든 뭐든 해서 갖다가 배열이나 리스트에 넣어
-
-// 순서대로 순번째랑 그다음번째랑 스케일 나누기2값  해서 방향 정한데다가 붙이ㅏ면 되자나
 
 public class gustnWKd123 : MonoBehaviour
 {
+    //일정갯수 맹글어서 그거 반반 찢어서 리스트에 넣는거?
+    public int how_Many_Make = 100;
+    GameObject 만든색기;
+    List<GameObject> 담을색기 = new List<GameObject>();
+    List<GameObject> 담을색기1 = new List<GameObject>();
+    List<GameObject> 담을색기2 = new List<GameObject>();
 
-    GameObject[] Toatkfkdgody;
 
     private void Start()
     {
-        Toatkfkdgody = GameObject.FindGameObjectsWithTag("Cube");
-
-        for (int i = 0; i < Toatkfkdgody.Length -1; i++)
+        for (int i = 0; i < how_Many_Make; i++)
         {
-            //인덱스 2번의 로컬스케일 Y를 둘로 나눈값을 저장한다
-            float a = Toatkfkdgody[i].transform.localScale.y / 2;
-            //인덱스 3번의 로컬스케일 Y를 둘로 나눈값을 저장한다
-            float b = Toatkfkdgody[i + 1].transform.localScale.y / 2;
-
-            
-            //새로 지정해줄 y포지션값을 저장할 변수
-            Vector3 wlqqhsownj = Vector3.up * (a + b);
-
-            Toatkfkdgody[0].transform.position = Toatkfkdgody[0].transform.parent.position;
-            //3번의 포지션을 2번과 똑같게 하고
-            Toatkfkdgody[i + 1].transform.position = Toatkfkdgody[i].transform.position;
-            //3번의 Y만 2개의 값을 둘로 나눠 합친값 만큼 더해준다
-            Toatkfkdgody[i + 1].transform.position += wlqqhsownj;
-
-
-            //만약에 Length보다 커졌으면 그만
-            if (Toatkfkdgody.Length < i + 1) break;
+            만든색기 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            만든색기.transform.position = transform.position;
+            담을색기.Add(만든색기);
         }
-    }
-    void Update()
-    {
-        
-    }
 
 
-    //다음에 할일
-    //스케일을 랜덤으로 지정해서 생성하기
-    //몇개?
-    //짝수로 10개 이상
-    //포지션을 붙이면서 리스트에 추가
-    //위아래 따로 리스트관리
-    
-    //플레이어의 높이와 부모 오브젝트의 높이차 비교
+
+    }
+
 }
