f85bd81eef5c77d5807cbbb6d84cbcfcd5cfa975
lsm9710 <54009854+lsm9710@users.noreply.github.com>
2020-05-11 13:46:44 +0900

    기둥하나 만드는거 완성

--- NightFury/Assets/SMFolder/Scripts/Algorithm Map/gustnWKd123.cs
+++ NightFury/Assets/SMFolder/Scripts/Algorithm Map/gustnWKd123.cs
@@ -2,31 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
-
-
-public class gustnWKd123 : MonoBehaviour
+//뽝씨쓰 생성 해서 담고 붙이기
+public class gustnWkd123 : MonoBehaviour
 {
-    //일정갯수 맹글어서 그거 반반 찢어서 리스트에 넣는거?
-    public int how_Many_Make = 100;
-    GameObject 만든색기;
-    List<GameObject> 담을색기 = new List<GameObject>();
-    List<GameObject> 담을색기1 = new List<GameObject>();
-    List<GameObject> 담을색기2 = new List<GameObject>();
-
-
-    private void Start()
+    List<GameObject> cubes = new List<GameObject>();
+    GameObject make_Cube;
+    public int make_count;
+    public Material[] mat;
+    void Start()
     {
-        for (int i = 0; i < how_Many_Make; i++)
+
+        for (int i = 0; i < make_count; i++)
         {
-            만든색기 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            만든색기.transform.position = transform.position;
-            담을색기.Add(만든색기);
+            make_Cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            make_Cube.transform.position = transform.position;
+            make_Cube.GetComponent<MeshRenderer>().material = mat[Random.Range(0, mat.Length)];
+            //make_Cube.GetComponent<MeshRenderer>().material.color = Random.ColorHSV();
+            make_Cube.transform.localScale = new Vector3(Random.Range(1, 5), Random.Range(1, 5), Random.Range(1, 5));
+            cubes.Add(make_Cube);
+            make_Cube = null;
         }
-
-
-
+        //위로 붙여욧
+        for (int i = 0; i < cubes.Count / 2; i++)
+        {
+            if (i + 1 >= cubes.Count)
+                break;
+            float a = cubes[i].transform.localScale.y / 2;
+            float b = cubes[i + 1].transform.localScale.y / 2;
+            Vector3 pos = Vector3.up * (a + b);
+            cubes[i + 1].transform.position = cubes[i].transform.position;
+            cubes[i + 1].transform.position += pos;
+        }
+        cubes[cubes.Count / 2].transform.position = cubes[0].transform.position;
+        float origin = cubes[0].transform.localScale.y / 2;
+        float newa = cubes[cubes.Count / 2].transform.localScale.y / 2;
+        Vector3 gustnWkd = Vector3.down * (origin + newa);
+        cubes[cubes.Count / 2].transform.position += gustnWkd;
+        //아래로 붙여욧
+        for (int i = cubes.Count / 2; i < cubes.Count; i++)
+        {
+            if (i + 1 >= cubes.Count)
+                break;
+            float a = cubes[i].transform.localScale.y / 2;
+            float b = cubes[i + 1].transform.localScale.y / 2;
+            Vector3 pos = Vector3.down * (a + b);
+            cubes[i + 1].transform.position = cubes[i].transform.position;
+            cubes[i + 1].transform.position += pos;
+        }
     }
 
 }
