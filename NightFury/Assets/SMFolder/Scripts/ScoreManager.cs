f3a05293c8c639841e70509253d523a0d45060e0
lsm9710 <54009854+lsm9710@users.noreply.github.com>
2020-05-18 10:52:18 +0900

    작업내용 합치기 완료

--- NightFury/Assets/SMFolder/Scripts/ScoreManager.cs
+++ NightFury/Assets/SMFolder/Scripts/ScoreManager.cs
@@ -14,8 +14,6 @@ public class ScoreManager : MonoBehaviour
 
     public static string scoreSaveText = "TopScore";
 
-    public static ScoreManager instance;
-
     public int Score
     {
         get
